fd9713378fd9889649fea32f804a086e92836f2c
EduardoChale <eduardochale88@gmail.com>
2025-10-30 23:36:49 -0500

    Report chapter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words,count_character
+from stats import count_words,count_character,sorting_list
+import sys
 
 def get_book_text(file_path):
     content=None
@@ -9,9 +10,20 @@ def get_book_text(file_path):
 
 
 def main():
-    book_content=get_book_text("./books/frankenstein.txt")
+    book_content=None
+    if len(sys.argv) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_content=get_book_text(sys.argv[1])
     number_words=count_words(book_content)
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}...\n----------- Word Count ----------")
     print(f'Found {number_words} total words')
+    print("--------- Character Count -------")
     number_characters=count_character(book_content)
-    print(number_characters)
+    #print(number_characters)
+    sorted_list=sorting_list(number_characters)
+    for character in sorted_list:
+        if character["char"].isalpha():
+            print(f'{character["char"]}: {character["num"]}')
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,16 @@ def count_character(content):
         else:
             character_dict[character]=1
     return character_dict 
+
+def sort_on(list_to_sort):
+    return list_to_sort["num"]
+
+def sorting_list(unsorted_list):
+    sorted_list=[]
+    for character in unsorted_list:
+        temp_dict={}
+        temp_dict["char"]=character
+        temp_dict["num"]=unsorted_list[character]
+        sorted_list.append(temp_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
